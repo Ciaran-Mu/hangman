85000d42c5dffe3d177531230fee41d4e367fe74
Ciaran Mullan <ciaranmullan12@gmail.com>
2023-10-03 15:01:59 +0100

    Updates README and reorganises contents of milestone_3.py into functions

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Hangman
+
+## Introduction
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. This game is written completely in Python, and represents an exercise in combining some key basic concepts of Python (as part of the AiCore course).
+
+## Installation
+
+To install this hangman game clone and enter the repository
+```
+git clone https://github.com/Ciaran-Mu/hangman.git
+cd hangman
+```
+Then run the most recent mielstone file
+```
+python milestone_3.py
+```
--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,28 @@
 # Import required modules 
 import random
 
+# Define functions
+def check_guess(guess):
+    guess = guess.lower()
+    # Check if the guessed letter is in the chosen word
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    # Loop to continuously ask user for a letter input and check if it is valid
+    while True:
+        guess  = input("Please enter your guess of one letter: ")
+        # Check the validity of the guess
+        if guess.isalpha() and len(guess) == 1:
+            # Guess is valid (a single letter in the alphabet)
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    # Use the acquired letter guess as arguement for the check_guess function
+    check_guess(guess)
+
 # Define a list of 5 fruits as possible words for hangman
 word_list = ["Apple", "Banana", "Orange", "Blueberry", "Raspberry"]
 print(word_list)
@@ -8,18 +30,4 @@ print(word_list)
 # Choose one of these words from the list at random
 word = random.choice(word_list)
 
-# Loop to continuously ask user for a letter input and check if it is valid
-while True:
-    guess  = input("Please enter your guess of one letter: ")
-    # Check the validity of the guess
-    if guess.isalpha() and len(guess) == 1:
-        # Guess is valid (a single letter in the alphabet)
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-
-# Check if the guessed letter is in the chosen word
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+ask_for_input()
